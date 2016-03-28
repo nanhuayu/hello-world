113a2c7f06207167bfce5080bd14a3e55d10ae5e
nanhuayu <nanhuayu@gmail.com>
2016-03-28 22:08:37 +0800

    Create pc08.py

---
+++ pc08.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import urllib
+import urllib2
+import re
+import bz2
+
+'''
+http://www.pythonchallenge.com/pc/def/integrity.html
+'''
+
+un = 'BZh91AY&SYA\xaf\x82\r\x00\x00\x01\x01\x80\x02\xc0\x02\x00 \x00!\x9ah3M\x07<]\xc9\x14\xe1BA\x06\xbe\x084'
+pw = 'BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08'
+
+acount = bz2.decompress(un)
+password = bz2.decompress(pw)
+
+print acount
+print password
