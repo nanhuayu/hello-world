71e0aabb13b2dc2ef0d461475159fbabdb96a3a7
nanhuayu <nanhuayu@gmail.com>
2016-10-30 10:35:33 +0800

    remark

--- Tools/remark.bat
+++ Tools/remark.bat
@@ -1,19 +1,11 @@
-﻿rem 20161025
-rem 生成文件备注信息
-
 @echo off
-echo.&echo.
-set /p it=输入文件夹的描述信息:
-
-rem 删除原有desktop.ini
+set /p it=input comments on the document:
+echo.&echo. 
 attrib %1\desktop.ini -s -h
 del %1\desktop.ini
-
-rem 生成新desktop.ini
 echo [.ShellClassInfo]>%1\desktop.ini
 echo InfoTip=%it%>>%1\desktop.ini
 attrib %1\desktop.ini +h +s
 attrib %1 +s
-
-rem 设置文件夹属性
+@echo on
 echo.&echo.
