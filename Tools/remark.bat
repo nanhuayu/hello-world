49af9805d53355e61d8b80172481b710e12d0343
nanhuayu <nanhuayu@gmail.com>
2016-10-30 10:39:53 +0800

    161030

--- Tools/remark.bat
+++ Tools/remark.bat
@@ -1,11 +1,16 @@
 @echo off
 set /p it=input comments on the document:
 echo.&echo. 
+
 attrib %1\desktop.ini -s -h
 del %1\desktop.ini
+
 echo [.ShellClassInfo]>%1\desktop.ini
 echo InfoTip=%it%>>%1\desktop.ini
+
 attrib %1\desktop.ini +h +s
 attrib %1 +s
+
 @echo on
+echo Success!
 echo.&echo.
